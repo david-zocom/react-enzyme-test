23b317461fd992c689606d15f38b41c19b203908
David Andersson <David Andersson>
2018-06-04 15:49:23 +0200

    minor fix

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import OnOff from './OnOff.js';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div>
-
+	  	<OnOff />
 	  </div>
     );
   }
